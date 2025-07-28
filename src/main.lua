deca75defc0d0caeb9674d101640492585a2969a
uptu <uptu@uptu.dev>
2025-07-28 04:20:14 -0700

    feat(core): import feature functions in main file
    modify:
    * `./src/main.lua`: add `./src/libs/feats.lua` to imports
    
    Signed-off-by: uptu <uptu@uptu.dev>

--- src/main.lua
+++ src/main.lua
@@ -2,10 +2,12 @@
 -- `./binding.lua`: A keybind object; stored in the BindMap
 -- `./bindmap.lua`: A keybind map for ease of lookup and management
 -- `./input.lua`: The Love2D event hook override functions
+-- `./feats.lua`: The main "feature" functions for the mod
 -- `./timers.lua`: A timer table for checking keypress duration
 assert(SMODS.load_file('src/libs/binding.lua'))();
 assert(SMODS.load_file('src/libs/bindmap.lua'))();
 assert(SMODS.load_file('src/libs/input.lua'))();
+assert(SMODS.load_file('src/libs/feats.lua'))();
 assert(SMODS.load_file('src/libs/timers.lua'))();
 
 --- BMAG mutable state.
