83ce225352cec6f85a2d12acd824ab95f20df6d2
uptu <uptu@uptu.dev>
2025-07-31 00:49:22 -0700

    docs(localization): add `en-us` localization documentation
    modify:
    * `./localization/en-us.lua`: document localization file to make future
      localization easier for contributors
    
    Signed-off-by: uptu <uptu@uptu.dev>

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1,10 +1,12 @@
 return {
     misc = {
         dictionary = {
-            -- Config Menu
+            -- Header text for the keybind menu
             keybinds = 'Keybinds',
+            -- Bind instructions
             bind_text = 'Click a feature, then press or hold a button to bind it to that key.',
 
+            -- Feature names (for keybind menu)
             multiselect = 'Multiselect (Hold only)',
             deselect = 'Deselect',
             sort_suit = 'Sort by suit',
@@ -13,10 +15,13 @@ return {
             discard = 'Discard current hand',
             restart = 'Restart run',
 
+            -- Bind state indicators
             click = '[CLICK]',
             hold = '[HOLD]',
             listening = '[LISTENING]',
             none = '[NONE]',
+
+            -- Save button label
             save = '[SAVE]'
         },
     },
